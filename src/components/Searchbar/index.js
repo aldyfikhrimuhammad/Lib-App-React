21c20e205a70f47508296e7538ccfa423fdf1be3
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-10-20 20:21:39 +0700

    Build Home Card [Still Static]

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,11 +1,12 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import '../../styles/Components/Searchbar/Searchbar.css'
 
 function Searchbar() {
   return (
     <>
       <FloatingLabel controlId="floatingInput" label="Search">
-        <Form.Control type="search" placeholder="Search" className="rounded-pill" />
+        <Form.Control type="text" placeholder="Search" className="rounded-pill search-bar" />
       </FloatingLabel>
     </>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,70 @@
-import React from 'react'
-import NavbarComp from '../../components/Navbar'
-import '../../styles/Pages/Home/Home.css'
-import CarouselComp from '../../components/Carousel'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Pages/Home/Home.css";
+import NavbarComp from "../../components/Navbar";
+import CarouselComp from "../../components/Carousel";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Laskar from "../../Assets/laskar Pelangi Portrait.jpeg";
+import Dilan from "../../Assets/Dilan 1990 Portrait.jpeg";
+import Harry from "../../Assets/Harry Potter Portrait.jpeg";
 
 const Home = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <NavbarComp />
       <Container className="mt-5">
-        <Row className='d-flex flex-column mb-3 justify-content-center align-items-center'>
-          <Col className='p-2 w-100 mt-5 border'>
-            <CarouselComp />
+        <Row className="d-flex flex-column mb-3 justify-content-center align-items-center">
+          <Col>
+          <h1 className="text-center">Disini Ada Carousel</h1>
+            {/* <CarouselComp /> */}
           </Col>
-          <Col className='p-2 w-100 mt-5 border'>
-            <h1>2nd content</h1>
-            <h2 className='ms-5 mb-3 text-start'>List Books</h2>
+          <Col className="p-2 w-100 mt-5">
+            <h2 className="ms-5 mb-2 text-start">List Books</h2>
             <Container>
-              <Row>
-                <Col className='border'>Card 1</Col>
-                <Col className='border'>Card 2</Col>
-                <Col className='border'>Card 3</Col>
+              <Row className="d-flex justify-content-center align-items-center">
+                <Col className="d-flex justify-content-center align-items-center mt-4">
+                  <Link to={"/detail"}>
+                    <Card style={{ width: "18rem" }} className="card-home">
+                      <Card.Img variant="top" src={Laskar} style={{ height: "20rem" }} />
+                      <Card.Body>
+                        <Card.Title>Laskar Pelangi</Card.Title>
+                        <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere eu est eu fermentum. Cras tempor mollis urna. Integer mattis, metus ut aliquet faucibus.</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
+                <Col className="d-flex justify-content-center align-items-center mt-4">
+                  <Link to={"/detail"}>
+                    <Card style={{ width: "18rem" }} className="card-home">
+                      <Card.Img variant="top" src={Dilan} style={{ height: "20rem" }} />
+                      <Card.Body>
+                        <Card.Title>Dilan 1990</Card.Title>
+                        <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere eu est eu fermentum. Cras tempor mollis urna. Integer mattis, metus ut aliquet faucibus.</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
+                <Col className="d-flex justify-content-center align-items-center mt-4">
+                  <Link to={"/detail"}>
+                    <Card style={{ width: "18rem" }} className="card-home">
+                      <Card.Img variant="top" src={Harry} style={{ height: "20rem" }} />
+                      <Card.Body>
+                        <Card.Title>Harry Potter</Card.Title>
+                        <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere eu est eu fermentum. Cras tempor mollis urna. Integer mattis, metus ut aliquet faucibus.</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
               </Row>
             </Container>
           </Col>
         </Row>
       </Container>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
